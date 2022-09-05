2a38373408529ccae119e2a0ba1e67267e739fb8
Carla Martinez <mgcarlasofia@gmail.com>
2022-09-05 15:58:03 -0300

    agregando ItemCount

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ItemCount from "../ItemCount/ItemCount";
+
+
 
 const ItemListContainer = ( props ) => {
     return(
         <div className="container">
             <p className="d-flex justify-content-center text-danger">{props.greeting}</p>
+           <ItemCount stock={5} initial={1} onAdd={0}  />
         </div>
     )
 };
